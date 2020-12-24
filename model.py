5a46047f20ca6350cf0068fa508f5a4a8170a245
vibhutimishra <vibhuti.mishra09@gmail.com>
2020-12-24 21:42:10 +0530

    INitial

---
+++ model.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+def function():
+    image = cv2.imread('C:\\Users\\Vibuthi mishra\\Documents\\projects\\DIP_Project\\demo.jpeg') 
+    # We use cvtColor, to convert to grayscale 
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+    
+    cv2.imshow('Grayscale', gray_image) 
+    cv2.waitKey(0)   
+    
+    # window shown waits for any key pressing event 
+    cv2.destroyAllWindows() 
+    preprocess(gray_image)
+
+def preprocess(bgr_img):#gray image   
+    img = bgr_img[:]
+    cv2.imshow('1', img)
+    cv2.waitKey()   
+
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    cv2.imshow('blur', blur) 
+    cv2.waitKey(0) 
+
+    # thesholding and binarization 
+    ret,th_img = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU) #converts black to white and inverse
+    print(ret)
+    cv2.imshow('th_img', th_img) 
+    cv2.waitKey(0)  
+
+    # Noise reduction
+    clean = cv2.fastNlMeansDenoising(th_img)
+    cv2.imshow('clean image', clean) 
+    cv2.waitKey(0) 
+    rows, cols = th_img.shape
+    bg_test = np.array([th_img[i][i] for i in range(5)])
+    print(bg_test)
+    if bg_test.all() == 0:
+        text_color = 255
+    else:
+        text_color = 0
+
+function()
